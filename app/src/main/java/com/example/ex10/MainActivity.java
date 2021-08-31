778d14d2f11961c437be4e3a179adf83ce842964
YEOKYEONGMIN <84887067+YEOKYEONGMIN@users.noreply.github.com>
2021-08-31 12:09:15 +0900

    10장 끝

--- app/src/main/java/com/example/ex10/MainActivity.java
+++ app/src/main/java/com/example/ex10/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.ex10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +14,9 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RatingBar;
 import android.widget.Toast;
+
+import java.io.File;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
     RadioButton second, third, rdoSum, rdoSub, rdoMul, rdoDiv;
@@ -28,14 +34,19 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.ex10_07);
 //        setContentView(R.layout.ex10_09);
 //        setContentView(R.layout.ex10_16);
-        setContentView(R.layout.ex10_3);
+//        setContentView(R.layout.ex10_3);
+//        setContentView(R.layout.ex10_20);
+        setContentView(R.layout.ex10_22);
+
 
 //        ex01();
 //        ex1();
 //        ex07();
 //        ex09();
 //        ex16();
-        ex3();
+//        ex3();
+//        ex21();
+        ex23();
     }
 
     void ex01(){
@@ -179,6 +190,91 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("Num2", Integer.parseInt(edtNum2.getText().toString()));
                 intent.putExtra("Calc",i);
                 startActivityForResult(intent,0);
+            }
+        });
+    }
+    void ex21(){
+        Button btnDial = (Button) findViewById(R.id.btnDial);
+        Button btnWeb = (Button) findViewById(R.id.btnWeb);
+        Button btnGoogle = (Button) findViewById(R.id.btnGoogle);
+        Button btnSearch = (Button) findViewById(R.id.btnSearch);
+        Button btnSms = (Button) findViewById(R.id.btnSms);
+        Button btnPhoto = (Button) findViewById(R.id.btnPhoto);
+
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:01012345678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        btnWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://www.hanbit.co.kr");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("http://maps.google.com/maps?q="+37.554264+","+126.913598);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, "안드로이드");
+                startActivity(intent);
+            }
+        });
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.putExtra("sms_body", "안녕하세요?");
+                intent.setData(Uri.parse("smsto:" + Uri.encode("010-1234-4567")));
+                startActivity(intent);
+            }
+        });
+
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                Uri uri = Uri.fromFile((new File(Environment.
+                        getExternalStorageDirectory().getPath()+"/jeju13.jpg")));
+                intent.setDataAndType(uri,"image/jpeg");
+                startActivity(intent);
+            }
+        });
+    }
+    void ex23(){
+        android.util.Log.i("액티비티 테스트", "onCreate()");
+
+        Button btnDial = (Button) findViewById(R.id.btnDial);
+        Button btnFinish = (Button) findViewById(R.id.btnFinish);
+
+        btnDial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:01012345678");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
